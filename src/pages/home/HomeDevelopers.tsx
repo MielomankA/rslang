df730ae73b78fc06a7b6c765a3f1b8f628c5a5b0
MielomankA <mielomanka@mail.ru>
2022-02-07 17:50:28 +0300

    feat: add DeveloperCard component

--- src/pages/home/HomeDevelopers.tsx
+++ src/pages/home/HomeDevelopers.tsx
@@ -1,47 +1,26 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { grey } from '@mui/material/colors';
-
 import './HomeDevelopers.scss';
 import logoMielo from '../../assets/image/logo-mielo.png';
+import { DeveloperCard } from './DeveloperCard';
 
 export const HomeDevelopers = () => {
   return (
     <section className="home__developers">
       <h2 className="home__developers_title">Наша команда</h2>
       <div className="home__developers_content">
-        <div className="developers__card">
-          <img src={logoMielo} alt="" className="developer__card_image" />
-          <div className="developer__card_description">
-            <div className="developer__card_name">
-              <h3>Mielomanka</h3>
-              <a href="https://github.com/MielomankA" target="_blank" rel="noreferrer noopener">
-                <GitHubIcon sx={{ color: grey[900] }} />
-              </a>
-            </div>
-            <p className="developer__card_job">Frontend-developer</p>
-            <p className="developer__card_work"> Что сделали ?</p>
-            <div className="developer__card_characteristic">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            </div>
-          </div>
-        </div>
-
-        <div className="developers__card">
-          <img src={logoMielo} alt="" className="developer__card_image" />
-          <div className="developer__card_description">
-            <div className="developer__card_name">
-              <h3>Mielomanka</h3>
-              <a href="https://github.com/MielomankA" target="_blank" rel="noreferrer noopener">
-                <GitHubIcon sx={{ color: grey[900] }} />
-              </a>
-            </div>
-            <p className="developer__card_job">Frontend-developer</p>
-            <p className="developer__card_work"> Что сделали ?</p>
-            <div className="developer__card_characteristic">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            </div>
-          </div>
-        </div>
+        <DeveloperCard
+          avatarUrl={logoMielo}
+          name="Mielomanka"
+          githubUrl="https://github.com/MielomankA"
+          jobStatus="Frontend-developer"
+          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."
+        />
+        <DeveloperCard
+          avatarUrl={logoMielo}
+          name="Mielomanka"
+          githubUrl="https://github.com/MielomankA"
+          jobStatus="Frontend-developer"
+          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."
+        />
       </div>
     </section>
   );
--- src/shared/ts/models.ts
+++ src/shared/ts/models.ts
@@ -2,3 +2,11 @@ export interface IAdvantageCardProps {
   title: string;
   description: string;
 }
+
+export interface IDeveloperCardProps {
+  avatarUrl: string;
+  name: string;
+  githubUrl: string;
+  jobStatus: string;
+  description: string;
+}
