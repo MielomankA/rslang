c44b66a87ee141a58120794d4d52efc836effc5d
MielomankA <79061120+MielomankA@users.noreply.github.com>
2022-02-20 17:36:06 +0300

    Textbook (#9)
    * feat: add basic layout, render cards, route burger-menu
    
    * feat: add pagination/groups words, WordCard component, refactor
    
    * feat: stop/start audio, refactor
    
    * refactor: remove comments
    
    * refactor: remove comment

---
+++ src/pages/textbook/Textbook.tsx
@@ -0,0 +1,66 @@
+import axios from 'axios';
+
+import { useEffect, useState } from 'react';
+
+import { Pagination } from '@mui/material';
+import HearingIcon from '@mui/icons-material/Hearing';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+
+import { Footer } from '../../components/footer/Footer';
+import { Header } from '../../components/header/Header';
+import { IWord } from '../../shared/ts/models';
+
+import './Textbook.scss';
+import { GameCard } from './GameCard';
+import { TextbookGroupsButtons } from './TextbookGroupsButtons';
+import { WordCard } from './WordCard';
+
+export const Textbook = () => {
+  const [wordsData, setWordsData] = useState<Array<IWord>>([]);
+  const [group, setGroup] = useState<number>(0);
+  const [page, setPage] = useState<number>(0);
+
+  const urlWords = `${process.env.REACT_APP_BASE_URL}words?group=${group}&page=${page}`;
+
+  useEffect(() => {
+    axios
+      .request({
+        url: urlWords,
+        method: 'GET',
+      })
+      .then((response) => {
+        setWordsData(response.data);
+      });
+  }, [urlWords]);
+
+  return (
+    <>
+      <Header />
+      <section className="textbook-page">
+        <div className="textbook-page__games">
+          <GameCard text="Аудиовызов">
+            <HearingIcon />
+          </GameCard>
+          <GameCard text="Спринт">
+            <DirectionsRunIcon />
+          </GameCard>
+        </div>
+        <TextbookGroupsButtons setGroup={setGroup} />
+        <Pagination
+          className="textbook-page__pagination"
+          count={30}
+          variant="outlined"
+          shape="rounded"
+          color="primary"
+          onChange={(_, page) => setPage(page - 1)}
+        />
+        <div className="textbook-page__words">
+          {wordsData.map((word, index) => {
+            return <WordCard key={`word-card-${index}`} wordInfo={word} />;
+          })}
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
